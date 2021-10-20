7b463e16213dcac33da43c967af67d2924a0c2f5
“sarthak <sarthakgangrade0@gmail.com>
2021-10-21 04:37:30 +0530

    add harmonic_number

---
+++ harmonic_number/harmonic_number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonic_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0.0;
+            Console.WriteLine("Enter the Value For Harmonic");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                result += 1 / (float)i;
+                Console.WriteLine("1/{0} + ", i);
+            }
+
+            Console.WriteLine($"={result}");
+        }
+    }
+}
