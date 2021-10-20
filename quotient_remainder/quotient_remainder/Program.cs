03322b61f3420a1180ca1e80410a71d2fe53f4e0
“sarthak <sarthakgangrade0@gmail.com>
2021-10-21 04:48:31 +0530

    add quotient remainder

---
+++ quotient_remainder/quotient_remainder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quotient_remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Dividend = 0;
+            int Divisor = 0;
+
+            Console.Write("Enter the Dividend: ");
+            Dividend = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Divisor: ");
+            Divisor = int.Parse(Console.ReadLine());
+
+            int Quotient = Dividend / Divisor;
+            int Reminder = Dividend % Divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor{1}", Dividend, Divisor);
+            Console.WriteLine("Quotient: " + Quotient);
+            Console.WriteLine("Reminder: " + Reminder);
+        }
+    }
+}
