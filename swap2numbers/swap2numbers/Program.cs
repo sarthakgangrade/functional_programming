fb4dda7654cd30df5eb9089a131c689c04443786
“sarthak <sarthakgangrade0@gmail.com>
2021-10-21 04:53:33 +0530

    add swap2num

---
+++ swap2numbers/swap2numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap2numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber = 0;
+            int SecondNumber = 0;
+            int temp = 0;
+            Console.Write("Enter First Number: ");
+            FirstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+            SecondNumber = int.Parse(Console.ReadLine());
+
+            temp = FirstNumber;
+            FirstNumber = SecondNumber;
+            SecondNumber = temp;
+
+            Console.WriteLine("After Swapping : ");
+            Console.WriteLine("First Number : " + FirstNumber);
+            Console.WriteLine("Second Number : " + SecondNumber);
+        }
+    }
+}
